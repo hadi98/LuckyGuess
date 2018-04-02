15ef55ec93d01549828533518569dfef57789f37
Hadi Bastanfar <98bastanfar@gmail.com>
2018-04-02 22:50:53 +0430

    We added(Game.java): #Increment function. #Toast that shows the incremented number.

--- app/src/main/java/com/hadibastanfar/luckyguess/Entry.java
+++ app/src/main/java/com/hadibastanfar/luckyguess/Entry.java
@@ -1,7 +1,6 @@
 package com.hadibastanfar.luckyguess;
 
-import android.content.Intent;
-import android.graphics.Color;
+import  android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,14 +10,12 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
 public class Entry extends AppCompatActivity {
 
     //githubtest
     //dafaq
-
-//shaghz
+    //shaghz
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
